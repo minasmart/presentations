3ae499ce384ffc043b48bf2253d4eb7f84a0e9b4
Mina Smart <mina.smart@shopify.com>
2016-02-04 16:57:37 -0500

    Clean up stream threads

--- rails-girls-to-action-cable/twitter-streaming/server/app/channels/twits_channel.rb
+++ rails-girls-to-action-cable/twitter-streaming/server/app/channels/twits_channel.rb
@@ -6,16 +6,19 @@ class TwitsChannel < ApplicationCable::Channel
     filter = params[:filter]
     stream_name = "tweet_stream_#{filter}"
     stream_from stream_name
-    TwitterStream.stream filter do |tweet|
-      ActionCable.server.broadcast stream_name, {
-        id: tweet.id,
-        source: tweet.source,
-        uri: tweet.uri.to_s,
-        full_text: tweet.full_text
-      }
+    @stream_thread = Thread.new do
+      TwitterStream.stream filter do |tweet|
+        ActionCable.server.broadcast stream_name, {
+          id: tweet.id,
+          source: tweet.source,
+          uri: tweet.uri.to_s,
+          full_text: tweet.full_text
+        }
+      end
     end
   end
 
   def unsubscribed
+    @stream_thread.exit
   end
 end
